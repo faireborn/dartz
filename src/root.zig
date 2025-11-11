37ed011c3b26fad97e12969a73a03ceb4a9542f5
Ryuto Saito <r.saito0111@gmail.com>
2025-11-11 14:30:06 +0900

    refactor: remove unused std import

--- src/root.zig
+++ src/root.zig
@@ -1,8 +1,6 @@
 pub const DoubleArray = @import("dartz.zig").DoubleArray;
 pub const DoubleArrayImpl = @import("dartz.zig").DoubleArrayImpl;
 
-const std = @import("std");
-
 test {
     _ = @import("dartz.zig");
 }
